27bb95fc904389e900c09996e5226f741441f041
Phil <philip.broadhead@hotmail.co.uk>
2019-11-28 16:55:22 +0000

    initial commits to the gatsby playground

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default () => (
+    <div style={{ color: `purple` }}>
+        <h1>Testing Site</h1>
+        <p>This is a test site for the Gatsby Platform using React</p>
+    </div>
+)
